b0baa7ddc028adde6827d32c6c7a24926e03b9d4
18380447973 <zimeng.zhang@monstar-lab.com>
2016-06-03 18:04:59 +0800

    zxc

--- ChoiceCourseSystem/NewViewController.m
+++ ChoiceCourseSystem/NewViewController.m
@@ -11,8 +11,16 @@
 #import "MBProgressHUD.h"
 
 @interface NewViewController ()<UITableViewDelegate,UITableViewDataSource,MBProgressHUDDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *newsTableview;
 
 @property (nonatomic, strong)MBProgressHUD *hub;
+
+@property (nonatomic, strong)NSArray *newsArr;
+
+@property (nonatomic, strong)NSMutableArray *collegeArr;
+
+@property (nonatomic, strong)NSMutableArray *collegeNum;
+
 @end
 
 @implementation NewViewController
@@ -43,6 +51,63 @@
     [NetHelper postRequest:kURL_selectable withActionStr:@"news" withDataStr:@"{\"year\":\"\"}" withNetBlock:^(id responseObject) {
         
         NSLog(@"responseObject___%@",responseObject);
+        _newsArr = [NSArray arrayWithArray:[responseObject objectForKey:@"teachingschedules"]];
+        _collegeArr = [NSMutableArray array];
+        _collegeNum = [NSMutableArray array];
+        
+        for (int i = 0 ; i < _newsArr.count; i++)
+        {
+            NSString *time = [_newsArr[i]objectForKey:@"time"];
+            NSString *date = [time componentsSeparatedByString:@" "][0];
+        }
+        
+        for (int i = 0; i < _newsArr.count; i++)
+        {
+            if (i==0)
+            {
+                NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+                [dic setObject:[[_newsArr[i] objectForKey:@"time"] componentsSeparatedByString:@" "][0] forKey:@"time"];
+                [dic setObject:[_newsArr[i] objectForKey:@"collegename"] forKey:@"collegename"];
+                [_collegeArr addObject:dic];
+                NSLog(@"_collegeArr   :%@",_collegeArr);
+            }
+            else
+            {
+                BOOL notNave = YES;
+                for (int j = 0; j<_collegeArr.count; j++)
+                {
+                    if ([[_newsArr[i] objectForKey:@"collegename"] isEqualToString:[_collegeArr[j] objectForKey:@"collegename"]]&&[[[_newsArr[i] objectForKey:@"time"] componentsSeparatedByString:@" "][0] isEqualToString:[_collegeArr[j] objectForKey:@"time"]])
+                    {
+                        notNave = NO;
+                    }
+                    
+                }
+                if (notNave)
+                {
+                    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+                    [dic setObject:[[_newsArr[i] objectForKey:@"time"] componentsSeparatedByString:@" "][0] forKey:@"time"];
+                    [dic setObject:[_newsArr[i] objectForKey:@"collegename"] forKey:@"collegename"];
+                    [_collegeArr addObject:dic];
+                }
+            }
+        }
+        NSLog(@"%@",_collegeArr);
+        for (int i = 0; i < _collegeArr.count; i++)
+        {
+            int num = 0;
+            for (int j = 0;j < _newsArr.count; j++)
+            {
+                if ([[_newsArr[j] objectForKey:@"collegename"] isEqualToString:[_collegeArr[i] objectForKey:@"collegename"]]&&[[[_newsArr[j] objectForKey:@"time"] componentsSeparatedByString:@" "][0] isEqualToString:[_collegeArr[i] objectForKey:@"time"]])
+                {
+                    num ++;
+                }
+            }
+            [_collegeNum addObject:[NSString stringWithFormat:@"%d",num]];
+        }
+        NSLog(@"%@",_collegeNum);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [_newsTableview reloadData];
+        });
     } withErrBlock:^(id err) {
         
     }];
@@ -75,20 +140,17 @@
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 3;
+    return _collegeArr.count;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    switch (indexPath.row)
+    switch ([_collegeNum[indexPath.row] integerValue])
     {
-        case 0:
+        case 1:
             return 210;
             break;
-        case 1:
-            return 260;
-            break;
         case 2:
-            return 300;
+            return 260;
             break;
         default:
             break;
@@ -98,45 +160,117 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+//    //cell1
+//    @property (weak, nonatomic) IBOutlet UILabel *timeLab1;
+//    @property (weak, nonatomic) IBOutlet UILabel *collegeName1;
+//    @property (weak, nonatomic) IBOutlet UILabel *fristLab1;
+//    @property (weak, nonatomic) IBOutlet UIButton *infoBtn1;
+//    
+//    
+//    
+//    //cell2
+//    @property (weak, nonatomic) IBOutlet UILabel *timeLab2;
+//    @property (weak, nonatomic) IBOutlet UILabel *collegeName2;
+//    @property (weak, nonatomic) IBOutlet UILabel *fristLab2;
+//    @property (weak, nonatomic) IBOutlet UILabel *secondLab2;
+//    @property (weak, nonatomic) IBOutlet UIButton *infoBtn2;
+//    
+//    
+//    //cell3
+//    
+//    @property (weak, nonatomic) IBOutlet UILabel *timeLab3;
+//    @property (weak, nonatomic) IBOutlet UILabel *collegeName3;
+//    @property (weak, nonatomic) IBOutlet UILabel *fristLab3;
+//    @property (weak, nonatomic) IBOutlet UILabel *secondLab3;
+//    @property (weak, nonatomic) IBOutlet UILabel *trirdLab3;
+//    @property (weak, nonatomic) IBOutlet UIButton *infoBtn3;
     NewTableViewCell *cell;
-    switch (indexPath.row)
+    switch ([_collegeNum[indexPath.row] integerValue])
     {
-        case 0:
+        case 1:
         {
             cell = [tableView dequeueReusableCellWithIdentifier:@"cell1"];
             if (!cell)
             {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"NewTableViewCell" owner:nil options:nil][0];
             }
+            cell.timeLab1.text = [_collegeArr[indexPath.row] objectForKey:@"time"];
+            cell.collegeName1.text = [_collegeArr[indexPath.row] objectForKey:@"collegename"];
+            for (int i = 0; i<_newsArr.count; i++)
+            {
+                if ([cell.collegeName1.text isEqualToString:[_newsArr[i] objectForKey:@"collegename"]]&&[cell.timeLab1.text isEqualToString:[[_newsArr[i] objectForKey:@"time"] componentsSeparatedByString:@" "][0]])
+                {
+                    cell.fristLab1.text = [_newsArr[i] objectForKey:@"coursename"];
+                }
+            }
         }
             break;
-        case 1:
+        case 2:
         {
             cell = [tableView dequeueReusableCellWithIdentifier:@"cell2"];
             if (!cell)
             {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"NewTableViewCell" owner:nil options:nil][1];
             }
+            cell.timeLab2.text = [_collegeArr[indexPath.row] objectForKey:@"time"];
+            cell.collegeName2.text = [_collegeArr[indexPath.row] objectForKey:@"collegename"];
+            int haveCourseNum = 0;
+            for (int i = 0; i<_newsArr.count; i++)
+            {
+                if ([cell.collegeName2.text isEqualToString:[_newsArr[i] objectForKey:@"collegename"]]&&[cell.timeLab2.text isEqualToString:[[_newsArr[i] objectForKey:@"time"] componentsSeparatedByString:@" "][0]])
+                {
+                    haveCourseNum ++;
+                    if (haveCourseNum==1)
+                    {
+                        cell.fristLab2.text = [_newsArr[i] objectForKey:@"coursename"];
+                    }
+                    else
+                    {
+                        cell.secondLab2.text = [_newsArr[i] objectForKey:@"coursename"];
+                    }
+                }
+            }
         }
             
             break;
-            
-        case 2:
+        default:
         {
             cell = [tableView dequeueReusableCellWithIdentifier:@"cell3"];
             if (!cell)
             {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"NewTableViewCell" owner:nil options:nil][2];
             }
+            cell.timeLab3.text = [_collegeArr[indexPath.row] objectForKey:@"time"];
+            cell.collegeName3.text = [_collegeArr[indexPath.row] objectForKey:@"collegename"];
+            int haveCourseNum = 0;
+            for (int i = 0; i<_newsArr.count; i++)
+            {
+                if ([cell.collegeName3.text isEqualToString:[_newsArr[i] objectForKey:@"collegename"]]&&[cell.timeLab3.text isEqualToString:[[_newsArr[i] objectForKey:@"time"] componentsSeparatedByString:@" "][0]])
+                {
+                    haveCourseNum ++;
+                    if (haveCourseNum==1)
+                    {
+                        cell.fristLab3.text = [_newsArr[i] objectForKey:@"coursename"];
+                    }
+                    else if(haveCourseNum==2)
+                    {
+                        cell.secondLab3.text = [_newsArr[i] objectForKey:@"coursename"];
+                        NSLog(@"%@",[_newsArr[i] objectForKey:@"coursename"]);
+                        NSLog(@"%@",cell.secondLab3.text);
+                    }
+                    else
+                    {
+                        cell.trirdLab3.text = [_newsArr[i] objectForKey:@"coursename"];
+                    }
+                    if (haveCourseNum>=3)
+                    {
+                        break;
+                    }
+                }
+            }
         }
-            
-            break;
-            
-        default:
             break;
     }
-    
-   
     
     return cell;
 }
